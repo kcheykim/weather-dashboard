3ea203124091e9112af83094f2d7bb5caa4a8315
kcheykim <kosalcheykim@gmail.com>
2021-10-23 21:07:48 -0700

    search found the data for the 5 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,60 @@
-//var apiKey = "&appid=7fc2d608ebf66e206f3145c2c83cb073";
-//var apiUrl ="https://api.openweathermap.org/data/2.5/forecast?q=";
-var counter = 0;
+var apiKey = "&appid=7fc2d608ebf66e206f3145c2c83cb073";
+var apiUrl ="https://api.openweathermap.org/data/2.5/forecast?q=";
 
 var submitBtnEl = $(".searchBtn"); //save button for input txt
-
-
-////fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=7fc2d608ebf66e206f3145c2c83cb073")
-//console.log(apiUrl + cityName + apiKey);
-// fetch("apiUrl" + cityName + "apiKey")
-
-
-//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-
-// fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=fd5967fd42c5816f972f3c2953622952")
-// .then(function(response){return response.json()})
-// .then(function(data){console.log(data)
-//   console.log(data.name);
-// });
+var cityListEl = $(".city-container");
+var infoEl = $(".info5")
+var weather = {};
 
 function searchCity(event) {
     event.preventDefault();
-    var path = $(this).parent(".city-container").siblings(".main-info").children(".info5").children(".day1");
-    var cityName = $(this).siblings(".input-city").val();
-    localStorage.setItem(counter, cityName);
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=7fc2d608ebf66e206f3145c2c83cb073")
+    var list;
+   // var path = $(this).parent(".city-container").siblings(".main-info").children(".info5").children(".day1");
+    var cityName = $("#city").val();
+    debugger;
+
+    if(cityName != "") {storeNames(cityName, list);}
+
+    fetch(apiUrl + cityName + apiKey)
         .then(function(response){
-            if(response.ok){
-                return response.json().then(function(data) {
-                    for(var i =0; i < data.length; i++) {
-                        path = data.id;
-                        alert(path);
-                        console.log(path);
+            response.json().then(function(data) {
+                // console.log(data);
+                // console.log(data.list);
+                var list = data.list;
+                var num = 3;
+                for(var i = 0; i < list.length; i++) {
+                  
+                    if( i == num) {
+                        console.log(list[i].dt_txt);
+                        var cityWeather = {
+                            date: list[i].dt_txt,
+                            icon: list[i].weather[0].icon,
+                            temp: list[i].main.temp,
+                            wind: list[i].wind.speed,
+                            humid: list[i].main.humidity
+                        }
+                    localStorage.setItem(cityName, JSON.stringify(cityWeather));
+                    num = num + 8;
                     }
-                });
-            }
+                }
+            });
         });
-        
-    counter++;
 }
+
+function storeNames(theCityName, theList) {
+    var cName = $('<button class="cityBtn" type="submit"></button>').text(theCityName);
+    cityListEl.append(cName);
+
+    for(var j = 0; j < 5; j++) {
+        var data = $('<div class="day"></div>').text(localStorage.getItem("thecityName"));
+        $(".day5").append(data);
+
+        console.log(data);
+    }
+}
+
+    
+
+
 
 submitBtnEl.on("click", searchCity);
