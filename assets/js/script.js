29248b713d08d91b80862123b0f7bec1a04a93f7
kcheykim <kosalcheykim@gmail.com>
2021-10-25 14:46:59 -0700

    adding formatting to dates and icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,104 @@
-var apiKey = "&appid=7fc2d608ebf66e206f3145c2c83cb073";
+var apiKey = "&units=imperial&appid=7fc2d608ebf66e206f3145c2c83cb073";
 var apiUrl5 = "https://api.openweathermap.org/data/2.5/forecast?q=";
 var apiUrl1 = "https://api.openweathermap.org/data/2.5/weather?q=";
 var apiIcon = "https://openweathermap.org/img/wn/";
-var unit = "&units=imperial";
+var imgUrl = "https://openweathermap.org/img/wn/";
 
 var submitBtnEl = $(".searchBtn"); //save button for input txt
 
-var infoEl = $(".fiveCol");
-var d = $(".days");
-var data = {};
-var dayArray = [];
-function searchCity(event) {
+
+// var d = $(".days");
+// var data = {};
+// var dayArray = [];
+function displayWeather(event) {
     event.preventDefault();
-    var list, date, icon, temp, wind, humid;
 
-    if($("#city") == "") {return;}
-    var cityName = $("#city").val();
+    var cityName = ($("#city").val());
+    if(!cityName) { return; }
+    if($(".currDay").children().length > 0) {
+        $(".currDay").empty();
+        currDay(cityName);
+    } else {  currDay(cityName); }
 
-   currDay(cityName);
-
-    fetch(apiUrl5 + cityName + unit + apiKey)
-        .then(function(response){
-            response.json().then(function(data) {
-
-                var list = data.list;
-                var num = 4;
-                for(var i = 0; i < list.length; i++) {
-    
-                    if( i === num) {
-                       // console.log(list[i].dt_txt);
-                            //var newCol = $('<div class= col-2></div>');
-                        
-                            var date = $('<p class="col"></p>').text("Date: " + list[i].dt); 
-                            var icon = $('<p class="col"></p>').text("Icon: " + list[i].weather[0].icon); 
-                            var temp = $('<p class="col"></p>').text('Temp: ' + list[i].main.temp +' F'); 
-                            var wind = $('<p class="col"></p>').text('Wind: ' + list[i].wind.speed + 'MPH'); 
-                            var humid = $('<p class="col"></p>').text('Humidity: ' + list[i].main.humidity + '%'); 
-                    
-                    
-                       infoEl.append(date, icon, temp, wind, humid);
-                        num = num + 8;
-                    }                 
-
-                }
-            });
-        });
-        displayData(cityName);
+    if($(".fiveCol").children().length > 0) {
+        $(".fiveCol").empty();
+        fiveDay(cityName);
+    } else { fiveDay(cityName); }
+    cityList(cityName);
 }
 
-function currDay(tName) {
-    $(".city-name").text(tName);
-    fetch(apiUrl1 + tName + unit + apiKey)
+function fiveDay(tname){
+    var infoEl = $(".fiveCol");
+    fetch(apiUrl5 + tname + apiKey)
     .then(function(response){
         response.json().then(function(data) {
-
-            var le = data;
-            console.log(le.dt);
-                    
-            var date = $('<p class="col"></p>').text("Date: " + le.dt); 
-            var icon = $('<p class="col"></p>').text("Icon: " + le.weather[0].icon); 
-            var temp = $('<p class="col"></p>').text('Temp: ' + le.main.temp +' F'); 
-            var wind = $('<p class="col"></p>').text('Wind: ' + le.wind.speed + 'MPH'); 
-            var humid = $('<p class="col"></p>').text('Humidity: ' + le.main.humidity + '%'); 
-                
-                
-                  $(".currDay").append(date, icon, temp, wind, humid);
-                
+            var list = data.list;
+            var num = 4;
+            for(var i = 0; i < list.length; i++) {
+                if( i === num) {
+                    var changeDt = moment.unix(list[i].dt).format("MM/DD/YY");
+                    var date = $('<h3 class="row"></h3>').text("Date: " + changeDt); 
+                    var iconUrl = apiIcon + list[i].weather[0].icon  + "@2x.png";
+                    var icon = $('<img>').attr('src', iconUrl);
+                    var temp = $('<p class="col"></p>').text('Temp: ' + list[i].main.temp +' F'); 
+                    var wind = $('<p class="col"></p>').text('Wind: ' + list[i].wind.speed + 'MPH'); 
+                    var humid = $('<p class="col"></p>').text('Humidity: ' + list[i].main.humidity + '%');                     
+                    infoEl.append(date, icon, temp, wind, humid);
+                    num += 8;
+                }                 
+            }
         });
     });
 }
-function displayData(theName) {
+
+function currDay(tName) {
+    fetch(apiUrl1 + tName + apiKey)
+    .then(function(response){
+        response.json().then(function(data) {
+            var normDt = moment.unix(data.dt).format("MM/DD/YY");
+            var date = $('<h2 class="col"></h2>').text(tName + " (" + normDt + ")"); 
+            var iUrl = apiIcon + data.weather[0].icon  + "@2x.png";
+            var icon = $('<img>').attr('src', iUrl);
+            var temp = $('<p class="col"></p>').text('Temp: ' + data.main.temp +' F'); 
+            var wind = $('<p class="col"></p>').text('Wind: ' + data.wind.speed + 'MPH'); 
+            var humid = $('<p class="col"></p>').text('Humidity: ' + data.main.humidity + '%');     
+             $(".currDay").append(date, icon, temp, wind, humid);
+        });
+    });
+}
+
+function cityList(theName) {
     var cityListEl = $(".city-container");
     var infoEl = $(".info5");
-    var cName = $('<button class="cityBtn w-100 h-25" type="submit"></button>').text(theName);
-  //  console.log(theCityName);
+    var nString, cName;
+   
+ debugger;
+    for(var a = 0; a < localStorage.length; a++) {
+        console.log(localStorage.getItem(a));
+        debugger;
+        if((localStorage.getItem(a)) != theName) {
+            console.log(localStorage.getItem(a));
+            debugger;
+           nString = '<button class="cityBtn" type="submit"></button>';
+            localStorage.setItem(theName, nString);
+            debugger;
+            cName = $(nString).text(theName);
+            cityListEl.append(cName);
+            debugger;
+        } else {
+            
+            nstring = localStorage.getItem(a).val();
+            cName = $(nString).text(theName);
+            cityListEl.append(cName);
+            debugger;
+        }
+
+    }
+
+    // $(".cityBtn").on("click", displayWeather);
+    nString = '<button class="cityBtn" type="submit"></button>';
+    cName = $(nString).text(theName);
     cityListEl.append(cName);
 }
 
-
-
-submitBtnEl.on("click", searchCity);
-//$(".cityBtn").on("click", dummyFunc);
+submitBtnEl.on("click", displayWeather);
